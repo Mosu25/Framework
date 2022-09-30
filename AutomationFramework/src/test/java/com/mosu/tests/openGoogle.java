c8d1e1cf5ab0d694f1333237a923027320220212
Mosu25 <mosu.avms@gmail.com>
2022-10-01 02:02:10 +0530

    open Google search amazon and Filpkart

--- AutomationFramework/src/test/java/com/mosu/tests/openGoogle.java
+++ AutomationFramework/src/test/java/com/mosu/tests/openGoogle.java
@@ -2,6 +2,8 @@ package com.mosu.tests;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -16,6 +18,17 @@ public class openGoogle {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Amazon",Keys.ENTER);
+		driver.quit();
+	}
+
+	public void searchFlipkart() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Filpkart",Keys.ENTER);
 		driver.quit();
 	}
 
