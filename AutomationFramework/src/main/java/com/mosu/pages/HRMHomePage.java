de01c9f174e65cb2afdde0ac70c2e6e9226e5a89
Mosu25 <mosu.avms@gmail.com>
2022-10-04 23:56:54 +0530

    Adding HRMHomePage

---
+++ AutomationFramework/src/main/java/com/mosu/pages/HRMHomePage.java
@@ -0,0 +1,16 @@
+package com.mosu.pages;
+
+import org.openqa.selenium.By;
+
+public class HRMHomePage {
+
+	
+	private final By admin = By.xpath("//span[text()='Admin']");
+
+	public By getAdmin() {
+		return admin;
+	}
+
+
+
+}
